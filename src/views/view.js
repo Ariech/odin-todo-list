3026181b4373eaf66f83e43be4b4dbbe9bf8067f
Dominik <dominikant99@gmail.com>
2023-09-13 13:08:29 +0200

    Create functions to create and get HTML elements

--- src/views/view.js
+++ src/views/view.js
@@ -2,4 +2,18 @@ const printTasksFromProject = (project) => {
   project.tasks.forEach((ele) => console.log(ele.getInfo()));
 };
 
-export { printTasksFromProject };
+const createElement = (tag, className) => {
+  const element = document.createElement(tag);
+
+  if (className) {
+    element.classList.add(className);
+  }
+
+  return element;
+};
+
+const getElement = (selector) => {
+  return document.querySelector(selector);
+};
+
+export { printTasksFromProject, createElement };
